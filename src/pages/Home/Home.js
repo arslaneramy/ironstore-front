c4c4632288243c42c66592d29e2c1efd89640a2f
Mati Weber <mati.weber@gmail.com>
2021-03-05 13:35:43 -0300

    recive changes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import Product from "./../../components/Products/Product";
+import axios from "axios";
 
-function Home() {
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const bringProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    bringProducts();
+  }, []);
   return (
     <div>
-      <h1>Welcome to Ironstore</h1>
+      <h1>Most popular products</h1>
+      {products.map((product) => (
+        <h3>{product.name}</h3>
+      ))}
     </div>
   );
-}
+};
 
 export default Home;
