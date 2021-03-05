e1d00f77f4db4bd3ed4464b2f29a91367d2cd37b
Arslane Gharout <iamarslane@gmail.com>
2021-03-05 17:31:15 +0100

    add some styling and fetch product list in home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+// import Product from "./../../components/Products/Product";
+import axios from "axios";
 
-function Home() {
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const bringProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+    };
+
+    bringProducts();
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to Ironstore</h1>
+      <h1>Most popular products</h1>
+      {products.map((product) => (
+        <>
+        <h3>{product.name}</h3>
+        <h3>{product.img}</h3>
+        <h3>{product.description}</h3>
+        <h3>{product.price}</h3>
+        <h3>{product.reviews}</h3>
+        <h3>{product.countInStock}</h3>
+      </>
+      ))}
     </div>
   );
-}
+};
 
 export default Home;
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { withAuth } from './../../context/auth-context';
-
 class Signup extends Component {
   state = { 
   email: "" ,
@@ -10,58 +9,44 @@ class Signup extends Component {
   lastName:"" ,
   shippingAddress:"" 
  };
-
-  
-
   handleFormSubmit = event => {
     event.preventDefault();
     const { email, password, firstName, lastName, shippingAddress } = this.state;
-    
     this.props.signup( email, password, firstName, lastName, shippingAddress);
   };
-
   handleChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
   };
-
   render() {
     const { email, password, firstName, lastName, shippingAddress } = this.state;
     return (
       <div>
-        <h1>Sign Up</h1>
-
-        <form onSubmit={this.handleFormSubmit}>
-
-          <label>email:</label>
-          <input type="text" name="email" value={email} onChange={this.handleChange} />
-
-          <label>Password:</label>
-          <input type="password" name="password" value={password} onChange={this.handleChange} />
-
-          <label>First Name:</label>
-          <input type="text" name="firstName" value={firstName} onChange={this.handleChange} />
-
-          <label>Last Name:</label>
-          <input type="text" name="lastName" value={lastName} onChange={this.handleChange} />
-
-          <label>shipping Address:</label>
-          <input type="text" name="shippingAddress" value={shippingAddress} onChange={this.handleChange} />
-
-          <input type="submit" value="Signup" />
+        <h1 className="signup" >Sign Up</h1>
+        <form className="form" onSubmit={this.handleFormSubmit}>
+          <div className="" >
+            <label>E-mail:</label>
+            <input type="text" name="email" placeholder="Enter E-mail" value={email} onChange={this.handleChange} />
+            <label>Password:</label>
+            <input type="password" name="password" placeholder="Enter Password" value={password} onChange={this.handleChange} />
+            <label>First Name:</label>
+            <input type="text" name="firstName" placeholder="Enter name" value={firstName} onChange={this.handleChange} />
+            <label>Last Name:</label>
+            <input type="text" name="lastName" placeholder="Enter Last Name" value={lastName} onChange={this.handleChange} />
+            <label>shipping Address:</label>
+            <input type="text" name="shippingAddress" placeholder="Enter Address" value={shippingAddress} onChange={this.handleChange} />
+            <input type="submit" value="Signup" />
+            </div>
+            <div>
+          <br></br>
+            <p> Already have account? </p>
+            <Link to={"/login"}> Login </Link>
+          </div>
         </form>
-        
-        <p>Already have account?</p>
-        <Link to={"/login"}> Login</Link>
       </div>
     );
   }
 }
-
-
-
 export default withAuth(Signup);
-
-
 // const EnhancedSignup = withAuth(Signup)
 // export default EnhancedSignup;
